e8922d245a707d2a47667a217b03041995e920bd
dimasprastyoaditama <prastyod647@gmail.com>
2025-06-26 15:57:09 +0700

    menambahkan metode search untuk class BinaryTree

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -24,5 +24,21 @@ class BinaryTree {
         ROOT = nullptr; //
     }
 
+    void search(int element, Node *&parent, Node *&currentNode)
+    {
+        //
+        //
+        currentNode = ROOT;
+        parent = nullptr;
+        while ((currentNode != nullptr) && (currentNode->info != element))
+        {
+            parent = currentNode;
+            if (element < currentNode->info)
+                currentNode = currentNode->leftchild;
+            else
+                currentNode = currentNode->rightchild;
+        }  
+    }
+
     
 }
